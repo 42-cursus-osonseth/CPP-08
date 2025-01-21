94b5de4a4b13aed2be11cc93dd876d2df1d6d6e1
Osonseth <max.cesaria@gmail.com>
2025-01-21 22:35:20 +0100

    ex02 ready for eval

--- ex02/Includes/MutantStack.hpp
+++ ex02/Includes/MutantStack.hpp
@@ -10,9 +10,8 @@ public:
     MutantStack();
     ~MutantStack();
 
-    typedef typename std::stack<T>::container_type::iterator it_type;
-
-    it_type begin();
+    typename MutantStack<T>::iterator begin();
+    typename MutantStack<T>::iterator end();
 
     class iterator
     {
@@ -22,6 +21,13 @@ public:
     public:
         iterator();
         ~iterator();
+        iterator(typename std::stack<T>::container_type::iterator it);
+        T &operator*();
+        iterator &operator--();
+        iterator &operator++();
+        iterator &operator--(int);
+        iterator &operator++(int);
+        bool operator!=(const iterator &other) const;
     };
 };
 
@@ -35,10 +41,54 @@ template <typename T>
 MutantStack<T>::iterator::iterator() : it() {}
 
 template <typename T>
+MutantStack<T>::iterator::iterator(typename std::stack<T>::container_type::iterator it) : it(it) {}
+
+template <typename T>
 MutantStack<T>::iterator::~iterator() {}
 
 template <typename T>
-typename MutantStack<T>::it_type MutantStack<T>::begin()
+typename MutantStack<T>::iterator MutantStack<T>::begin()
 {
-    return this->c.begin();
+    return iterator(this->c.begin());
+}
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+    return iterator(this->c.end());
+}
+template <typename T>
+T &MutantStack<T>::iterator::operator*()
+{
+    return *it;
+}
+template <typename T>
+typename MutantStack<T>::iterator &MutantStack<T>::iterator::operator--()
+{
+    --it;
+    return *this;
+}
+template <typename T>
+typename MutantStack<T>::iterator &MutantStack<T>::iterator::operator++()
+{
+    ++it;
+    return *this;
+}
+template <typename T>
+typename MutantStack<T>::iterator &MutantStack<T>::iterator::operator--(int)
+{
+    iterator tmp = *this;
+    --it;
+    return *tmp;
+}
+template <typename T>
+typename MutantStack<T>::iterator &MutantStack<T>::iterator::operator++(int)
+{
+    iterator tmp = *this;
+    ++it;
+    return *tmp;
+}
+template <typename T>
+bool MutantStack<T>::iterator::operator!=(const iterator &other) const
+{
+    return it != other.it;
 }
